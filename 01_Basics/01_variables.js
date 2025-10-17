611873295f3114e037f73155dc32605021034c8e
zeeshanshah0208 <zeeshanshah0208@gmail.com>
2025-10-17 06:00:45 +0000

    config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const AccountID = 144556
+let accountEmail = "zeeshanshah@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+
+accountEmail = "zeeshan@gmail.com"
+accountPassword = "647926"
+accountCity = "Jaipur"
+
+console.log(AccountID);
+
+console.table([AccountID, accountEmail, accountPassword, accountCity]);
+
