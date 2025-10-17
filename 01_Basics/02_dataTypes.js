149c72dd4ceb40f670f11fe044f220eb037e3dbe
zeeshanshah0208 <zeeshanshah0208@gmail.com>
2025-10-17 06:14:32 +0000

    config done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log(3 + 3);
+
+console.log("zeeshan");
+
+let name = "zeeshan"
+let age = 18
+let isLoggedIn = false
+
+console.log(name);
+console.log(age);
+console.log(isLoggedIn);
+
+
